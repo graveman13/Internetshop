77b3c47e9fb8745f7cb4308a9ea827f07be497af
Grave <graveman13@gmail.com>
2020-01-20 14:02:23 +0300

    delete HttpSession session

--- src/main/java/mate/academy/internetshop/controller/LogoutController.java
+++ src/main/java/mate/academy/internetshop/controller/LogoutController.java
@@ -12,8 +12,6 @@ public class LogoutController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        HttpSession session = req.getSession(true);
-        session.setAttribute("userId", "");
         req.getSession().invalidate();
         for (Cookie cookie : req.getCookies()) {
             if (cookie.getName().equals("MATE")) {
